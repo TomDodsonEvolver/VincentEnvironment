c69fb3d2630ddad969ff939eab3abc3315c1f019
Tom Dodson <tom.dodson@idea-evolver.com>
2017-03-09 08:07:42 -0500

    working build!

--- custom_platform/builder/setup-scripts/02-install-pkgs.sh
+++ custom_platform/builder/setup-scripts/02-install-pkgs.sh
@@ -63,5 +63,3 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A1
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-3.4.list
 apt-get update
 apt-get install -y mongodb-org
-service mondgod start
-
